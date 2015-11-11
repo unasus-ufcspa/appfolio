2bbc4aa6e2dbee715adf69e2b989ea8df8737842
UNASUS <des.unasus.ufcspa@gmail.com>
2015-11-11 12:09:48 -0200

    DataBase

---
+++ app/src/main/java/com/ufcspa/unasus/appportfolio/database/DataBaseAdapter.java
@@ -0,0 +1,40 @@
+package com.ufcspa.unasus.appportfolio.database;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by UNASUS on 11/11/2015.
+ */
+public class DataBaseAdapter {
+    /*
+        DataBaseAdapter source;
+        source = new DataBaseAdapter(getApplicationContext());
+        Log.d("BANCO", "TABELAS = " + source.getTestData());
+     */
+//    private SQLiteDatabase db;
+//    private DataBaseHelper helper;
+//
+//    public DataBaseAdapter(Context context)
+//    {
+//        helper = new DataBaseHelper(context);
+//        db = helper.getDatabase();
+//    }
+//
+//    public String getTestData()
+//    {
+//        String sql ="SELECT name FROM sqlite_master WHERE type='table'";
+//
+//        Cursor mCur = db.rawQuery(sql, null);
+//        String tabelas = "";
+//        if (mCur.moveToFirst())
+//        {
+//            do {
+//                tabelas += mCur.getString(0) + "\n";
+//            }while(mCur.moveToNext());
+//        }
+//        return tabelas;
+//    }
+
+}
