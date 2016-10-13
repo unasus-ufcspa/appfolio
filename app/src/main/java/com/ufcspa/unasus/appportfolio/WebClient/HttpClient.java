5b299e3e41b33ace97fd54bc353d12c9854d08f9
Zago <rfzago@gmail.com>
2016-10-13 10:34:39 -0300

    Alteração no HttpClient para mudanças rápidas na variável "ip".

--- app/src/main/java/com/ufcspa/unasus/appportfolio/WebClient/HttpClient.java
+++ app/src/main/java/com/ufcspa/unasus/appportfolio/WebClient/HttpClient.java
@@ -6,8 +6,8 @@ import android.content.Context;
  * Created by icaromsc on 29/03/2016.
  */
 public class HttpClient {
-    //    private static final String ip = "folio.unasus.ufcspa.edu.br";
-    public static final String ip = "192.168.0.9";//"192.168.0.9"; //200.18.67.9t
+    //public static final String ip = "folio.unasus.ufcspa.edu.br"; //PRODUÇÃO
+    public static final String ip = "192.168.0.9"; //TESTE
     protected static final String URL = "http://" + ip + "/webfolio/app_dev.php/";
     protected String tag="JSON";
     private Context context;
