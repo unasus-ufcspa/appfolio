36f708a3397ec54f4c5ee166be894d0c8744972a
Steffano <hsteffano@gmail.com>
2017-01-31 09:16:48 -0200

    Versão com finalização de versão e recebimento de dt_conclusion do servidor para aluno e tutor.

--- app/src/main/java/com/ufcspa/unasus/appportfolio/Activities/Fragments/FragmentReport.java
+++ app/src/main/java/com/ufcspa/unasus/appportfolio/Activities/Fragments/FragmentReport.java
@@ -6,21 +6,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
+import android.widget.TextView;
 
 import com.ufcspa.unasus.appportfolio.Adapter.SelectReportClassAdapter;
 import com.ufcspa.unasus.appportfolio.Model.PortfolioClass;
 import com.ufcspa.unasus.appportfolio.Model.Singleton;
+import com.ufcspa.unasus.appportfolio.Model.StudFrPortClass;
 import com.ufcspa.unasus.appportfolio.R;
 import com.ufcspa.unasus.appportfolio.database.DataBaseAdapter;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class FragmentReport extends Frag {
     private GridView grid_reports;
+    private TextView grid_empty;
     private DataBaseAdapter source;
     private Singleton singleton;
     private List<PortfolioClass> portclasses;
+    private ArrayList<PortfolioClass> finalList;
+    private ArrayList<StudFrPortClass> list;
+    private ArrayList<StudFrPortClass> listActivities;
 
     public FragmentReport() {
         // Required empty public constructor
@@ -43,17 +50,28 @@ public class FragmentReport extends Frag {
     private void init() {
         singleton = Singleton.getInstance();
         source = DataBaseAdapter.getInstance(getActivity());
+        finalList = new ArrayList<>();
+        grid_empty = (TextView) getView().findViewById(R.id.grid_empty);
 
         try {
             portclasses = source.selectListClassAndUserType(singleton.user.getIdUser());
+            for (PortfolioClass portclass: portclasses){
+                list = source.selectListActivitiesAndStudentsByStudent(portclass.getIdPortClass(), portclass.getPerfil(), singleton.user.getIdUser());
+                listActivities = source.selectFinalizedActivitiesAndStudentsByStudent(portclass.getIdPortClass(), portclass.getPerfil(), singleton.user.getIdUser());
+                if (!list.isEmpty() && !listActivities.isEmpty() && list.size()==listActivities.size())
+                    finalList.add(portclass);
+            }
             Log.d("lista", "tam portlis:" + portclasses.size());
         } catch (Exception e) {
             Log.wtf("ERRO", e.getMessage());
         }
 
-        Collections.sort(portclasses);
-
-        SelectReportClassAdapter gridAdapter = new SelectReportClassAdapter(getActivity(), portclasses);
+        SelectReportClassAdapter gridAdapter = null;
+        if (!finalList.isEmpty()) {
+            grid_empty.setVisibility(View.INVISIBLE);
+            Collections.sort(finalList);
+            gridAdapter = new SelectReportClassAdapter(getActivity(), finalList);
+        }
         grid_reports = (GridView) getView().findViewById(R.id.grid_reports);
         grid_reports.setAdapter(gridAdapter);
     }
